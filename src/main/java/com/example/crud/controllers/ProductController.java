e767bf2edc4ad4771a3fc3c78b4e5786861111a2
Marcelo <11212101319@alunos.umc.br>
2025-03-26 01:17:42 -0300

    first commit

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -15,7 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Comparator;
 import java.util.stream.Collectors;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/product")
@@ -23,11 +25,46 @@ public class ProductController {
     @Autowired
     private ProductRepository repository;
 
-    @GetMapping
-    public ResponseEntity getAllProducts(){
-        var allProducts = repository.findAllByActiveTrue();
-        return ResponseEntity.ok(allProducts);
+    @GetMapping //endpoint1
+    public ResponseEntity<List<Product>> getAllProducts(@RequestParam(required = false) String category){
+        if (category != null) {
+            return ResponseEntity.ok(repository.findByCategoryAndActiveTrue(category));
+        }
+        return ResponseEntity.ok(repository.findAllByActiveTrue());
     }
+
+    @GetMapping("/{id}") //endpoint2
+    public ResponseEntity<Product> getProductById(@PathVariable String id) {
+        Optional<Product> optionalProduct = repository.findById(id);
+        return optionalProduct.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/top3-expensive") //endpoint3
+    public ResponseEntity<List<Product>> getTop3ExpensiveProducts() {
+        List<Product> top3 = repository.findAllByActiveTrue()//pegar todos os produtos ativos
+                .stream()
+                .sorted(Comparator.comparing(Product::getPrice).reversed()) //ordena do mais caro pro mais barto
+                .limit(3)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(top3);
+    }
+
+    @GetMapping("/grouped-sql")
+    public ResponseEntity<List<Product>> getProductsGroupedByCategorySQL() {
+        return ResponseEntity.ok(repository.findAllGroupedByCategoryOrderedByPrice());
+    }
+    @GetMapping("/grouped-java")
+    public ResponseEntity<Map<String, List<Product>>> getProductsGroupedByCategoryJava() {
+        List<Product> allProducts = repository.findAllByActiveTrue();
+
+        Map<String, List<Product>> grouped = allProducts.stream()
+                .sorted(Comparator.comparing(Product::getPrice)) // ordena por preço
+                .collect(Collectors.groupingBy(Product::getCategory)); // agrupa por categoria
+
+        return ResponseEntity.ok(grouped);
+    }
+
 
     @PostMapping
     public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data){
--- src/main/java/com/example/crud/domain/product/ProductRepository.java
+++ src/main/java/com/example/crud/domain/product/ProductRepository.java
@@ -9,4 +9,9 @@ import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
     List<Product> findAllByActiveTrue();
+    List<Product> findByCategoryAndActiveTrue(String category);
+
+    @Query("SELECT p FROM com.example.crud.domain.product.Product p WHERE p.active = true ORDER BY p.category, p.price ASC")
+    List<Product> findAllGroupedByCategoryOrderedByPrice();
+
 }
